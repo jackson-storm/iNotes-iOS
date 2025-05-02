0f150e44384acbd2ac075c49efee9a7c424c5e9d
Евгений Петрукович <evgeniy.petrukovich@icloud.com>
2025-05-02 23:26:40 +0300

    Fix assets

---
+++ iNotes/Core/Utils/AssetsConstants.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum AssetsConstants {
+    enum Images {
+        static let placeholder = "placeholder"
+        
+    }
+    
+    enum Colors {
+        static let accent = "AccentColor"
+        
+    }
+    
+    enum Icons {
+        
+    }
+} 
